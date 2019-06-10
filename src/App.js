15aaa75c9d8c2ca62efe8fdfca0de63244464c2a
dustyzietz <dustyzietz@gmail.com>
2019-06-09 15:55:43 -1000

    connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Die from './Die';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Die />
     </div>
   );
 }
---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Die extends Component {
+    render() {
+        return (
+            <div>
+               Works 
+            </div>
+        )
+    }
+}
+
+export default Die
